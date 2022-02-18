b5686a6d44505e846fbe726e828c94535d9b42c2
oreorange <92431652+oreorange@users.noreply.github.com>
2022-02-18 18:41:12 +0900

    [js7]

--- app.js
+++ app.js
@@ -24,30 +24,30 @@
 //   $('.box1').hide();
 // });
 
-$(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color':'#00f',
-      'width':'200px',
-      'height':'100px'
-    }).slideUp();
-  });
-});
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color':'#00f',
+//       'width':'200px',
+//       'height':'100px'
+//     }).slideUp();
+//   });
+// });
 
-$(function(){
-  $('.box2').mouseover(function(){
-    $('.box2').css({'background-color':'#0000FF'});
-  });
-})
+// $(function(){
+//   $('.box2').mouseover(function(){
+//     $('.box2').css({'background-color':'#0000FF'});
+//   });
+// })
 
-$(function(){
-  $('.box2').mouseover(function(){
-    $('.box2').css({'background-color': '#0000FF'});
-  });
-  $('.box2').mouseout(function(){
-    $('.box2').css({'background-color': '#FF0000'});
-  });
-});
+// $(function(){
+//   $('.box2').mouseover(function(){
+//     $('.box2').css({'background-color': '#0000FF'});
+//   });
+//   $('.box2').mouseout(function(){
+//     $('.box2').css({'background-color': '#FF0000'});
+//   });
+// });
 
 // マウス乗せた
 // $(function(){
@@ -59,11 +59,44 @@ $(function(){
 //   });
 // });
 
+// $(function(){
+//   $('.box2').on('click', function(){
+//     $('.box2').addClass('box2-ext');
+//   });
+//   $('.box2').mouseout(function(){
+//     $('.box2').removeClass('box2-ext');
+//   });
+// });
+
+
+/* 7章 this children */
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+// thisを使えば個別に指定しなくてもいい
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
 $(function(){
-  $('.box2').on('click', function(){
-    $('.box2').addClass('box2-ext');
-  });
-  $('.box2').mouseout(function(){
-    $('.box2').removeClass('box2-ext');
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
